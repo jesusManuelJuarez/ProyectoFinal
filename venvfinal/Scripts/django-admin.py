d4f383f993b031d4a75473995cbf400cf9a116a4
jesus manuel juarez acosta <chuya@JUAREZ-7VTVVI5N.JESUSMANUEL>
2020-04-03 09:41:05 -0600

    proyecto_final3Corte

---
+++ venvfinal/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\chuya\OneDrive\Documents\POLITECNICA\tareas\4 CUATRIMESTRE\PROGRAMACION WEB\profinal\venvfinal\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venvfinal/final/Dashboard/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class DashboardClass(LoginRequiredMixin,View):
+    templates_m = 'Dashboard/Dashboard.html'
+
+    def get (self,request,*args,**kwargs):
+        return render(request,self.templates_m,{})
+
+    
